38f7820f5a1698efdcc359119cfbebc98a7f981c
ValimGustavo <gustavo.souza.valim@gmail.com>
2020-11-17 14:36:24 -0300

    Correçao simples em dev

--- src/app/modules/user/login.module.ts
+++ src/app/modules/user/login.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginRoutingModule } from './login.routing';
-import { PerfilReadComponent } from './perfil/perfil-read/perfil-read.component';
-import { PerfilViewComponent } from './perfil/perfil-view/perfil-view.component';
+
 
 @NgModule({
-  declarations: [LoginComponent, PerfilReadComponent, PerfilViewComponent],
+  declarations: [LoginComponent ],
   imports: [CommonModule, FormsModule, ReactiveFormsModule, LoginRoutingModule ],
   exports: [],
 })
